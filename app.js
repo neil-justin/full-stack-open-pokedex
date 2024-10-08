6c6f4606c261533e8677c23a3141494582fa6666
Neil <mallarineil9@gmail.com>
2024-10-08 17:38:42 +0800

    Add workflow for automating deployment to fly.io

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ console.log('port', PORT)
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('hello version')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
